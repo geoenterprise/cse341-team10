244c3e5e2ec918124212ae3caef326dc5d6fe471
TheeJStar <kingoflightj@gmail.com>
2025-02-07 21:15:47 -0800

    test for making single controller, made a reusable function update

--- routes/facilities.js
+++ routes/facilities.js
@@ -10,6 +10,22 @@ routes.get('/:id', facilitiesController.getSingleFacility);
 // routes.post('/', isAuthenticated, validation.saveFacilities, facilitiesController.createFacility);
 // routes.put('/:id', isAuthenticated, validation.saveFacilities, facilitiesController.updateFacility);
 // routes.delete('/:id', isAuthenticated, facilitiesController.deleteFacility);
+routes.put(
+    '/test/:id', 
+    validation.saveEmployees, 
+    (req, res) => {
+        //#swagger.tags=['Employees']
+        facilitiesController.update(req, res, 'employees', {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            badgenumber: req.body.badgenumber,
+            department: req.body.department,
+            shift: req.body.shift,
+            location: req.body.location,
+            status: req.body.status
+        }
+    )}
+)
 routes.post('/', validation.saveFacilities, facilitiesController.createFacility);
 routes.put('/:id', validation.saveFacilities, facilitiesController.updateFacility);
 routes.delete('/:id', facilitiesController.deleteFacility);
