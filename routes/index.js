cbd26380e8ed4ee4dd2ab93f3b69180ace9910fd
TheeJStar <kingoflightj@gmail.com>
2025-02-13 21:55:17 -0800

    Risky push (to main): did some clean up

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const routes = require('express').Router();
-// const passport = require('passport');
 
 routes.use('/', require('./swagger'));
 
@@ -10,16 +9,5 @@ routes.use('/employees', require('./employees'));
 routes.use('/facilities', require('./facilities'));
 routes.use('/locations', require('./locations'));
 routes.use('/managers', require('./managers'));
-
-
-// Commented out for OAuth
-// routes.get('/login', passport.authenticate('github'), (req, res) => {});
-
-// routes.get('/logout', function(req, res, next) {
-//     req.logout(function(err) {
-//         if (err) { return next(err); }
-//         res.redirect('/');
-//     })
-// });
 
 module.exports = routes;
